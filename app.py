40060a1faad03ead4d7c686ad4aea07ce7e2ad9d
abenesc <55511933+abenesc@users.noreply.github.com>
2021-02-04 17:30:06 -0800

    json to excel test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-import urllib.request, json 
+import urllib.request, json
 import pandas as pd
 from sodapy import Socrata
 import xlsxwriter
@@ -14,13 +14,15 @@ json_array = []
 
 client = Socrata("data.sfgov.org", None)
 
+
 # Goes through list and puts each API endpoint as array entry
 def get_apis(array):
-    f = open(api,'r')
+    f = open(api, 'r')
     file = list(f)
     for line in file:
         line = line[32:-5]
         api_array.append(line)
+
 
 # Gets JSON from each API and puts it into api_array
 def get_json(api_url):
@@ -28,17 +30,17 @@ def get_json(api_url):
     json_array.append(results)
     print(type(results))
 
+
 # Takes json and creates excel
 def convert(array):
     with open('out.xlsv') as outfile:
         for row_num, data in enumerate(array):
-            writer = csv,writer(outfile)
+            writer = csv, writer(outfile)
             writer.writerows(data.iteritems())
-        
+
 
 # Takes json and creates excel (CURRENTLY BROKEN) 
 def convert_b(array):
-
     df = pd.DataFrame(array)
 
     writer = pd.ExcelWriter('pandas_simple2.xlsx', engine='xlsxwriter')
