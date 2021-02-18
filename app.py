82986d99da42cb9b32f8495385c10fa5445e3f4c
sagarika5646 <62318843+sagarika5646@users.noreply.github.com>
2021-02-17 17:06:15 -0800

    updated with JSON file, indexes

--- app.py
+++ app.py
@@ -1,57 +1,57 @@
-import requests
-import urllib.request, json 
-import pandas as pd
-from sodapy import Socrata
-import xlsxwriter
-import openpyxl
-from openpyxl import load_workbook
+import urllib.request
+import json
+import os.path
+from os import path
 import csv
 
-# File name of API master list
-api = 'record.txt'
-# Array of API, name of metric and parsecode
-api_array = []
-# Array of JSON entries
-json_array = []
+def get_data_from_records():
+    with open('record.json') as f:
+        list_of_entries = json.load(f)
+    return list_of_entries
 
-client = Socrata("data.sfgov.org", None)
-
-# Goes through file of API, 
-def get_apis(array):
-    f = open(api,'r')
-    file = list(f)
-    for line in file:
-        line_split = line.split(',')
-        api_array.append(line_split)
-        
-
-# Gets value needed by using parsecode from the data of API
-# api[3] = city
-# api[1] = metric name
-def get_value(api):
-    r = requests.get(api[0])
-    data = r.json()
-    parsecode = remove_quotes(api[2])
-    value = eval(parsecode)
-    city = api[3]
-    metric_name = api[1]
-    return value, metric_name, city
-
-# Removes quotes around string
-def remove_quotes(string):
-    value = string.replace("'","")
-    return value
-
-# Takes value and city as input and places data as a row in correct excel file
-def fill_excel(value, metric_name, city):
-    file_name = city + '.csv'
-    dict = {"Metric Name" : metric_name,
-            "Value"       : value} 
-    df = pd.DataFrame(dict, index=[0])
-    df.to_csv(file_name, mode='a', header=False)
-
+def get_data_from_single_entry(single_entry):
+    with urllib.request.urlopen(single_entry["api_endpoint"]) as url:
+        data = json.loads(url.read().decode())
+    if single_entry["metric_parse_code"] != "":
+        metric_value = eval(single_entry["metric_parse_code"])
+    else:
+        metric_value = "-"
+    if single_entry["date_parse_code"] != "":
+        date_value = eval(single_entry["date_parse_code"])
+    else:
+        date_value = "-"
+    return {'Serial No.': "",
+            'Metric Name': single_entry["metric_name"],
+            'City': single_entry["city"],
+            'Metric Value': metric_value,
+            'Date': date_value,
+            'CoV Dimension ID': single_entry["cov_dimesion_id"] if single_entry["cov_dimesion_id"] != "" else "-",
+            'CoV Metric Name' : single_entry["cov_metric_name"] if single_entry["cov_metric_name"] != "" else "-",
+            'API Endpoint': single_entry["api_endpoint"]
+    }
+    
+def put_single_entry_in_csv(data_dict):
+    filename = f"{data_dict['City']}.csv"
+    if not path.exists(filename):
+        with open(filename, 'w') as csvfile:
+            writer = csv.writer(csvfile, lineterminator='\n')
+            writer.writerow([key for key in data_dict.keys()])
+    with open(filename, 'r') as csvfile:
+        existing_data = csv.reader(csvfile)
+        next(existing_data, None)
+        existing_metrics = []
+        existing_cities = []
+        for row in existing_data:
+            existing_metrics.append(row[1])
+            existing_cities.append(row[2])
+        if data_dict['Metric Name'] not in existing_metrics or data_dict['City'] not in existing_cities:
+            with open(filename, 'a') as csvfile:
+                writer = csv.writer(csvfile, lineterminator='\n')
+                data_dict["Serial No."] = len(existing_metrics) + 1
+                writer.writerow([value for value in data_dict.values()])
+            
 if __name__ == "__main__":
-    get_apis(api)
-    for entry in api_array:
-        data = get_value(entry)
-        fill_excel(data[0], data[1], data[2])
+    list_of_entries = get_data_from_records()
+    for single_entry in list_of_entries:
+        data_dict = get_data_from_single_entry(single_entry)
+        put_single_entry_in_csv(data_dict)
